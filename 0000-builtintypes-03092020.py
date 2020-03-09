bc1829326ee4e0c0d1768c22d5b88f2791f102c4
Mike Slomczynski <mike.slomczynski@gmail.com>
2020-03-09 12:29:05 -0500

    Release Version 0000.002

---
+++ 0000-builtintypes-03092020.py
@@ -0,0 +1,18 @@
+#Hello
+#Lets get right into it
+#referance not a tutorial
+#built-in types
+#This information is found on docs.python.org
+
+#numerics, squences, mappings, classes, instances, exceptions
+
+#numerics = int, float, complex
+#squences = list, tuple, range
+#text squences = str
+#binary sequences = bytes, bytearray, memoryview
+#set = set, frozenset
+#mappings = dict
+
+#classes = 
+#instances = 
+#exceptions = 
